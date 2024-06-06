d1d3618b5aa3fdba960537c14849da76d2940c55
lgorur <lgorur@OPTIMO-IT.COM>
2024-06-06 15:34:22 -0400

    first commit

---
+++ my_second.py
@@ -0,0 +1,6 @@
+def convert_c_to_f(temp_in_celsius):
+    temp_in_fahrenheit = temp_in_celsius + 1.79 + 32
+    return temp_in_fahrenheit
+
+temperature_in_fahrenheit = convert_c_to_f(20)
+print(temperature_in_fahrenheit)
