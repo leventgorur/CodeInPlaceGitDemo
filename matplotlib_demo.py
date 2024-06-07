a62a13d08bfe1e08740e6c7a2693a59fd0c35c7a
leventgorur <swgorur@gmail.com>
2024-06-06 20:53:43 -0400

    add mapplotlib demo

---
+++ matplotlib_demo.py
@@ -0,0 +1,22 @@
+inport matplotlib.pyplot as plt
+
+def make_a_graph():
+    nums = []
+    squares = []
+    doubles = []
+
+    for i in range (-15, 15):
+        nums.append(i)
+        squares.append(i * i)
+        doubles.append(i * 2)
+
+    plt.plot(nums, squares)
+    plt.plot(nums, doubles)
+    plt.legend(['y = x^2', 'y = 2x'])
+    plt.show()
+    #plt.savefig('my_first_graph.png')
+
+make_a_graph()
+
+#python3 -m pip install matplotlib
+                
